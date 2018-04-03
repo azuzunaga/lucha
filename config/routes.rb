9d50643a534d2fa76e72185158e7082721fc1eb8
Americo <ame.zuz@gmail.com>
2018-04-03 16:21:39 -0700

    Create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :api, defaults: { format: :json } do
+    resource :user, only: [:create]
+    resource :session, only: [:create, :destroy, :show]
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#root'
 
