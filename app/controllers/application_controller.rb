2817c066af3477d99de4329ce4f6a342bd688dd6
Americo <ame.zuz@gmail.com>
2018-04-03 16:33:50 -0700

    Complete application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,33 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def login(user)
+    user.reset_session_token!
+    session[:session_token] = user.session_token
+    @current_user = user
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def logout
+    current_user.reset_session_token!
+    session[:session_token] = nil
+    @current_user = nil
+  end
+
+  private
+
+  def require_logged_in
+    unless current_user
+      render json: { base: ['You must be logged in'] }, status: 401
+    end
+  end
 end
