a203682ead66725a682b99417c79c091135b9fcf
Americo <ame.zuz@gmail.com>
2018-04-03 16:42:48 -0700

    Complete sessions controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,7 +1,21 @@
 class Api::SessionsController < ApplicationController
   def create
+    @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+
+    if @user
+      login(@user)
+      render "api/users/show"
+    else
+      render json: ["User does not exist or password is incorrect"], status: 401
+    end
   end
 
   def destroy
+    @user = current_user
+    if @user
+      logout
+    else
+      render json: ["Can't logout what isn't logged in"], status: 404
+    end
   end
 end
