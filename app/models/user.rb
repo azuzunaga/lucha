dcbc3a0e297d029390524eeb74882b77c1b6972a
Americo <ame.zuz@gmail.com>
2018-04-03 16:21:58 -0700

    Ammend User.find_by

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,7 +33,7 @@ class User < ApplicationRecord
   # has_one :image
 
   def self.find_by_credentials(username, password)
-    user = User.find_by(username: username, password: password)
+    user = User.find_by(username: username)
     user && is_password?(password) ? user : nil
   end
 
