71433be466cd1051c04548756b51541bfb8f2607
Americo <ame.zuz@gmail.com>
2018-04-03 16:10:32 -0700

    Complete user model, associations skeleton

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,54 @@ class User < ApplicationRecord
 
   after_initialize :ensure_session_token
 
-  
+  # has_many :activities
+  #
+  # has_many :routes,
+  #          source: :route,
+  #          foreign_key: :author_id
+  #
+  # has_many :comments,
+  #          source: :comment,
+  #          foreign_key: :commenter_id
+  #
+  # has_many :likes,
+  #          source: :like,
+  #          foreign_key: :liker_id
+  #
+  # has_many :follows,
+  #          source: :follow,
+  #          foreign_key: :follower_id
+  #
+  # has_many :followers,
+  #          source: :follow,
+  #          foreign_key: :followee_id
+  #
+  # has_one :image
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username, password: password)
+    user && is_password?(password) ? user : nil
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_diggest).is_password?(password)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_diggest = BCrypt::Password.create(password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
+
 end
