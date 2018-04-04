e62b25b12c935dd9c7a02c085ef57303183fa865
Americo <ame.zuz@gmail.com>
2018-04-03 20:29:17 -0700

    Refactor entry file to render Root component

--- frontend/lucha.jsx
+++ frontend/lucha.jsx
@@ -1,16 +1,15 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import * as SessionAPIUtil from './util/session_api_util';
-
 import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
 
+  //||\\
   window.getState = store.getState;
   window.dispatch = store.dispatch;
+  //||\\
 
   const rootEl = document.getElementById("root");
-
-  ReactDOM.render(<h1>Bienvenido a Lucha</h1>, rootEl);
+  ReactDOM.render(<Root store={store} />, rootEl);
 });
