5d89243ddfdd3e392a9b4ca68ecd9d075326408f
Americo <ame.zuz@gmail.com>
2018-04-03 21:26:02 -0700

    Update App to render greeting container

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
+import GreetingContainer from './greeting/greeting_container';
+
 const App = () => (
   <div>
-    <h1>Lucha - Track your activities</h1>
+    <header>
+      <h1>Lucha - Track your activities</h1>
+      <GreetingContainer />
+    </header>
   </div>
 );
 
