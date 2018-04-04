df828a5b7a864f20ec45ba1d283c158e1c465994
Americo <ame.zuz@gmail.com>
2018-04-03 21:25:37 -0700

    Create greeting component

---
+++ frontend/components/greeting/greeting.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { link } from 'react-router-dom';
+
+const sessionLinks = () => (
+  <nav className="login-signup-demo">
+    <Link to="/login">LOG IN</Link>
+    &nbsp;or&nbsp;
+    <Link to="/signup">Sign up</Link>
+    &nbsp;or&nbsp;
+    <Link to="/demo">Demo User</Link>
+  </nav>
+);
+
+const personalGreeting = (currentUser, logout) => (
+  <hgroup className="header-group">
+    <h2 className="header-name">Hi, {currentUser.first_name}</h2>
+    <button className="header-button" onClick={logout}>Log out</button>
+  </hgroup>
+);
+
+const Greeting = ({ currentUser, logout}) => (
+  currentUser ? personalGreeting(currentUser, logout) : sessionLinks()
+);
+
+export default Greeting;
