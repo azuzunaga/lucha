661bcdd6d9becedd6d5106db043e8d68263a43aa
Americo <ame.zuz@gmail.com>
2018-04-04 11:38:29 -0700

    Working log in form: nest user params under user

--- frontend/components/session_forms/login_form.jsx
+++ frontend/components/session_forms/login_form.jsx
@@ -19,7 +19,7 @@ class LoginForm extends React.Component {
 
   handleSubmit(e) {
     e.preventDefault();
-    const user = Object.assign({}, this.state);
+    const user = {user: Object.assign({}, this.state)};
     this.props.processForm(user);
   }
 
