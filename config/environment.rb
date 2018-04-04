19629ac40679d7bec667a230cf49e99cf79077ce
Americo <ame.zuz@gmail.com>
2018-04-04 09:16:18 -0700

    Add builder option to translate between snake and cammel case

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Use jbuilder to translate between snake and camel case
+Jbuilder.key_format camelize: :lower
