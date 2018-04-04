f50b7f17d70fe5a1f71e78463f0c19faea47fd11
Americo <ame.zuz@gmail.com>
2018-04-03 21:31:43 -0700

    Import react

--- frontend/lucha.jsx
+++ frontend/lucha.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
@@ -5,10 +6,10 @@ import Root from './components/root';
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
 
-  //||\\
+  //
   window.getState = store.getState;
   window.dispatch = store.dispatch;
-  //||\\
+  //
 
   const rootEl = document.getElementById("root");
   ReactDOM.render(<Root store={store} />, rootEl);
