62bdd65c88b19edb5011d93f2ae80a3644331772
Americo <ame.zuz@gmail.com>
2018-04-04 14:51:03 -0700

    Remove test code

--- frontend/lucha.jsx
+++ frontend/lucha.jsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
 
-// test
-import * as actions from './actions/session_actions';
-
 document.addEventListener('DOMContentLoaded', () => {
   let store;
   if (window.currentUser) {
