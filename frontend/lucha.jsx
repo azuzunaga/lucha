2e61e25c315c3823ff9067783d48b87a6c577ea8
Americo <ame.zuz@gmail.com>
2018-04-03 17:34:47 -0700

    Put util functions on window for testing

--- frontend/lucha.jsx
+++ frontend/lucha.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as SessionAPIUtil from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
   const rootEl = document.getElementById("root");
+
+  window.login = SessionAPIUtil.login;
+  window.logout = SessionAPIUtil.logout;
+  window.signup = SessionAPIUtil.signup;
+
   ReactDOM.render(<h1>Bienvenido a Lucha</h1>, rootEl);
 });
