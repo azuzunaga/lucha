8e7a85c0b4d14e5f5ca32ebf046c3fa70025d966
Americo <ame.zuz@gmail.com>
2018-04-03 20:12:07 -0700

    Create root reducer with session and errors reducers

--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -1,5 +1,3 @@
-// frontend/reducers/errors_reducer.jsx
-
 import { combineReducers } from 'redux';
 
 import sessionErrorsReducer from './session_errors_reducer';
---
+++ frontend/reducers/root_reducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+
+import sessionReducer from './session_reducer';
+import errorsReducer from './errors_reducer';
+
+const rootReducer = combineReducers({
+  session: sessionReducer,
+  errors: errorsReducer,
+});
