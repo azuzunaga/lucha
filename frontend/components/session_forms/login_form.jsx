439165b9614e19e1181681a78a40c34b437162fe
Americo <ame.zuz@gmail.com>
2018-04-04 09:59:55 -0700

    Create login form - username input only with placeholder text

---
+++ frontend/components/session_forms/login_form.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+class LoginForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  update(field) {
+    return e => this.setState({
+      [field]: e.currentTarget.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const user = Object.assign({}, this.state);
+    this.props.processForm(user);
+  }
+
+  renderErrors() {
+    return(
+      <ul>
+        {this.props.errors.map((error, i) => (
+          <li key={`error-${i}`}>
+            {error}
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
+  render() {
+    return (
+      <div className="login-form-container">
+        <form onSubmit={this.handleSubmit} className="login-form-box">
+          Log In
+          <br/>
+          {this.renderErrors()}
+          <div className="login-form">
+            <br/>
+            <input type="text"
+              value={this.state.username}
+              onChange={this.update('username')}
+              className="login-input"
+              placeholder="Your email"
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
