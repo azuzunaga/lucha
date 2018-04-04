155b393098ac54bcd3dc3ede1f75a894e72c5e23
Americo <ame.zuz@gmail.com>
2018-04-04 14:28:27 -0700

    Protect auth routes

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import {
+  Route,
+  Redirect,
+  Switch,
+  Link,
+  HashRouter
+} from 'react-router-dom';
 
 import GreetingContainer from './greeting/greeting_container';
+import LoginFormContainer from './session_forms/login_form_container';
+import SignupFormContainer from './session_forms/signup_form_container';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
 const App = () => (
   <div>
@@ -8,6 +19,10 @@ const App = () => (
       <h1>Lucha - Track your activities</h1>
       <GreetingContainer />
     </header>
+    <Switch>
+      <AuthRoute exact path="/login" component={LoginFormContainer} />
+      <AuthRoute exact path="/signup" component={SignupFormContainer} />
+    </Switch>
   </div>
 );
 
