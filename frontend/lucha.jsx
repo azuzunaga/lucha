ab0c28a4ab5ddc6eaeb8f3faca095fed842a83bb
Americo <ame.zuz@gmail.com>
2018-04-03 21:43:46 -0700

    add login to window for testing

--- frontend/lucha.jsx
+++ frontend/lucha.jsx
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
 
+// test
+import * as actions from './actions/session_actions';
+
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
 
   //
   window.getState = store.getState;
   window.dispatch = store.dispatch;
+  window.login = actions.login;
+
   //
 
   const rootEl = document.getElementById("root");
