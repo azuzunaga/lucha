813a9a01559fd363456cbf2fb9ce9e918a4cda2a
Americo <ame.zuz@gmail.com>
2018-04-04 14:28:57 -0700

    Return nill unless there is a session token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   private
 
   def current_user
+    return nil unless session[:session_token]
     @current_user ||= User.find_by(session_token: session[:session_token])
   end
 
