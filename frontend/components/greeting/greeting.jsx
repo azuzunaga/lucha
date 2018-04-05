af16ea39cac2905c00c44d19a9313ee943e4c4dd
Americo <ame.zuz@gmail.com>
2018-04-05 12:33:07 -0700

    Reflow html to accomodate logged in features

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -2,20 +2,48 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const sessionLinks = () => (
-  <nav className="login-signup-demo">
-    <h1 id="logo">LUCHA</h1>
-    <Link to="/login"
-      id="splash-login-button"
-      className="login-button"
-    >
-      LOG IN
-    </Link>
-  </nav>
+    <nav className="header-nav">
+
+      <h1 className="header-logo">
+        <a href="#">LUCHA</a>
+      </h1>
+
+      <ul className="header-list">
+        <li>
+          <Link to="/login" id="splash-login-button" className="login-button">
+            LOG IN
+          </Link>
+        </li>
+      </ul>
+
+    </nav>
 );
 
 const personalGreeting = (currentUser, logout) => (
   <hgroup className="header-group">
-    <h2 className="header-name">Hi, {currentUser.firstName}</h2>
+
+    <nav className="left-nav">
+      <h1 className="header-logo">
+        <a href="#">LUCHA</a>
+      </h1>
+
+      <ul>
+        <li id="activities-btn">
+          <Link to="/activities">My Activities</Link>
+        </li>
+        <li id="routes-btn">
+          <Link to="/routes">My Routes</Link>
+        </li>
+      </ul>
+    </nav>
+
+    <ul>
+      <li>
+        <h2 className="header-name">Hi, {currentUser.firstName}</h2>
+
+      </li>
+    </ul>
+
     <button className="header-button" onClick={logout}>Log out</button>
   </hgroup>
 );
