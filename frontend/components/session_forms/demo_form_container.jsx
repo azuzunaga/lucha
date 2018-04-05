f41600ed86abd2e9a2c5a671f3ba5e1e4b34fbef
Americo <ame.zuz@gmail.com>
2018-04-05 12:30:20 -0700

    Create demo form container

---
+++ frontend/components/session_forms/demo_form_container.jsx
@@ -0,0 +1,25 @@
+import { connect } from 'react-redux';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { login } from '../../actions/session_actions';
+import DemoForm from './demo_form';
+
+const mapStateToProps = ({ errors }) => {
+  return {
+    errors: errors.session,
+    signupLink: <Link to="/signup">Sign Up</Link>,
+    demoLink: <Link to="/demo">Demo User</Link>,
+    tourLink: <Link to="/tour">Take Tour</Link>,
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    processForm: user => dispatch(login(user)),
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(DemoForm);
