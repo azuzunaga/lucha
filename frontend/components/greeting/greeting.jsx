92e46eea2dc1b805411faed8c23a996cd7a3d1ba
Americo <ame.zuz@gmail.com>
2018-04-05 16:23:36 -0700

    Navbar elements created

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -4,13 +4,13 @@ import { Link } from 'react-router-dom';
 const sessionLinks = () => (
     <nav className="header-nav">
 
-      <h1 className="header-logo">
-        <a href="#">LUCHA</a>
+      <h1 className="left-nav">
+        <a href="#" className="header-logo">LUCHA</a>
       </h1>
 
-      <ul className="header-list">
-        <li>
-          <Link to="/login" id="splash-login-button" className="login-button">
+      <ul className="header-list right-nav">
+        <li className="header-button">
+          <Link to="/login" id="splash-login-button">
             LOG IN
           </Link>
         </li>
@@ -20,7 +20,7 @@ const sessionLinks = () => (
 );
 
 const personalGreeting = (currentUser, logout) => (
-  <hgroup className="header-group">
+  <hgroup className="header-nav">
 
     <nav className="left-nav">
       <h1 className="header-logo">
@@ -39,35 +39,36 @@ const personalGreeting = (currentUser, logout) => (
 
     <nav className="right-nav">
       <ul>
-        <li id="user-information">
-          <ul>
-            <li>
-              <h2 className="header-name">Hi, {currentUser.firstName}</h2>
-            </li>
-            <li>
-              <a href="" id="user-dropdown-btn">
-                <div className="user-nav-avatar">
-                  <i class="material-icons md-36">account_circle</i>
-                </div>
-                <div className="nav-avatar-chevron">
-                  <i class="material-icons md-18">keyboard_arrow_down</i>
-                </div>
-              </a>
-            </li>
-            <li>
-              <a href="">
-                <div>
-                  <i class="material-icons md-24">add_circle_outline</i>
-                </div>
-              </a>
-            </li>
-          </ul>
-
+        {/* <li id="user-information">
+        <ul> */}
+        <li>
+          <h2 className="header-name">Hi, {currentUser.firstName}</h2>
+        </li>
+        <li>
+          <a href="" id="user-dropdown-btn">
+            <div className="user-nav-avatar">
+              <i class="material-icons md-36">account_circle</i>
+            </div>
+            <div className="nav-avatar-chevron">
+              <i class="material-icons md-18">keyboard_arrow_down</i>
+            </div>
+          </a>
+        </li>
+        {/* </ul>
+        </li> */}
+        <li>
+          <a href="">
+            <div>
+              <i class="material-icons md-24">add_circle_outline</i>
+            </div>
+          </a>
+        </li>
+        <li>
+          <button className="header-button" onClick={logout}>Log out</button>
         </li>
       </ul>
     </nav>
 
-    <button className="header-button" onClick={logout}>Log out</button>
   </hgroup>
 );
 
