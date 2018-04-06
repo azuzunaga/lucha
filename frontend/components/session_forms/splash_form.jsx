c5b53be7e32ca14bf619411ab98e32fc4550ea78
Americo <ame.zuz@gmail.com>
2018-04-06 09:52:38 -0700

    HTML and CSS for splash page

--- frontend/components/session_forms/splash_form.jsx
+++ frontend/components/session_forms/splash_form.jsx
@@ -4,9 +4,21 @@ import { withRouter, Link } from 'react-router-dom';
 class SplashForm extends React.Component {
   render() {
     return (
-      <div className="splash-form">
-        <Link to="/signup">Sign Up</Link>
-        <Link to="/demo">Demo User</Link>
+      <div className="login-form-container splash-form-container">
+        <form className="login-form-box splash-form-box">
+          <Link className="login-form-button" to="/login" id="login">
+            Log In
+          </Link>
+          <Link className="login-form-button" to="/signup" id="demo">
+            Sign Up
+          </Link>
+          <label className="session-form-separator">or</label>
+
+          <Link className="login-form-button" to="/demo" id="demo">
+            Demo User
+          </Link>
+          {/* <button className="tour-button">Take Tour</button> */}
+        </form>
       </div>
     );
   }
