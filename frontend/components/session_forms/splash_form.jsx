7b96ffe033f95731d3e5808da0c3a60e218c1182
Americo <ame.zuz@gmail.com>
2018-04-06 13:06:54 -0700

    Add bg div

--- frontend/components/session_forms/splash_form.jsx
+++ frontend/components/session_forms/splash_form.jsx
@@ -4,21 +4,23 @@ import { withRouter, Link } from 'react-router-dom';
 class SplashForm extends React.Component {
   render() {
     return (
-      <div className="login-form-container splash-form-container">
-        <form className="login-form-box splash-form-box">
-          <Link className="login-form-button" to="/login" id="login">
-            Log In
-          </Link>
-          <Link className="login-form-button" to="/signup" id="demo">
-            Sign Up
-          </Link>
-          <label className="session-form-separator">or</label>
+      <div className="bg">
+        <div className="login-form-container splash-form-container">
+          <form className="login-form-box splash-form-box">
+            <Link className="login-form-button" to="/login" id="login">
+              Log In
+            </Link>
+            <Link className="login-form-button" to="/signup" id="demo">
+              Sign Up
+            </Link>
+            <label className="session-form-separator">or</label>
 
-          <Link className="login-form-button" to="/demo" id="demo">
-            Demo User
-          </Link>
-          {/* <button className="tour-button">Take Tour</button> */}
-        </form>
+            <Link className="login-form-button" to="/demo" id="demo">
+              Demo User
+            </Link>
+            {/* <button className="tour-button">Take Tour</button> */}
+          </form>
+        </div>
       </div>
     );
   }
