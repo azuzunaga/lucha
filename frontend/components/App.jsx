c33b8236f8a0c96ddf98a55086fe0b36754916d1
Americo <ame.zuz@gmail.com>
2018-04-07 13:59:08 -0700

    Add new protected routes

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -13,13 +13,13 @@ import LoginFormContainer from './session_forms/login_form_container';
 import SignupFormContainer from './session_forms/signup_form_container';
 import SplashForm from './session_forms/splash_form';
 import DemoFormContainer from './session_forms/demo_form_container';
-import Test from './session_forms/test';
-import NewRoute from './session_forms/new_route';
+import Test from './routes/test';
+import NewRoute from './routes/new_route';
 
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
 const App = () => (
-  <div>
+  <div id="app">
     <header>
       <GreetingContainer />
     </header>
@@ -27,8 +27,8 @@ const App = () => (
       <AuthRoute exact path="/login" component={LoginFormContainer} />
       <AuthRoute exact path="/signup" component={SignupFormContainer} />
       <AuthRoute exact path="/demo" component={DemoFormContainer} />
-      <ProtectedRoute path="/routes" component={Test} />
       <ProtectedRoute path="/routes/new" component={NewRoute} />
+      <ProtectedRoute path="/routes" component={Test} />
       <AuthRoute path="/" component={SplashForm} />
     </Switch>
   </div>
