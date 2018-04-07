cc59f1a5307602207985dd5d0d9375859db4e281
Americo <ame.zuz@gmail.com>
2018-04-06 17:01:45 -0700

    Validate presence of last name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :username, :password_digest, :session_token, :first_name,
+  validates :username, :password_digest, :session_token, :first_name, :last_name,
             presence: true
   validates :username, :session_token, uniqueness: true
   validates :password, length: { minimum: 6 , allow_nil: true }
