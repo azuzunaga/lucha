f6e428d4447cae5668c1c1cc4db830c2f2a21e8b
Americo <ame.zuz@gmail.com>
2018-04-07 12:37:07 -0700

    New components!

---
+++ frontend/components/session_forms/test.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { withRouter, Link } from 'react-router-dom';
+
+class Test extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>HELLLLLOOOOOOO</h1>
+      </div>
+    );
+  }
+}
+
+export default withRouter(Test);
