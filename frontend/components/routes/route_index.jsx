b6e00cdeaf022560d3a87841cd08c423ca9b6e09
Americo <ame.zuz@gmail.com>
2018-04-10 16:10:17 -0700

    Add more details

--- frontend/components/routes/route_index.jsx
+++ frontend/components/routes/route_index.jsx
@@ -11,11 +11,18 @@ class RouteIndex extends React.Component {
   render() {
     const routes = this.props.routes;
     return (
-      <div>
-        <h1>My Routes</h1>
-        <ul>
-          {routes.map(route => <RouteDetail key={route.id} route={route} />)}
-        </ul>
+      <div className="route-index-component">
+        <div className="route-index-header">
+          <h1>My Routes</h1>
+          <Link to="/routes/new" className="new-route-button">
+            Create New Route
+          </Link>
+        </div>
+        <div className="route-detail-container">
+          <ul>
+            {routes.map(route => <RouteDetail key={route.id} route={route} />)}
+          </ul>
+        </div>
       </div>
     );
   }
