6327e098aa1c08c2c9826705b9da9d2ea65c6bc8
Americo <ame.zuz@gmail.com>
2018-04-10 13:48:37 -0700

    Create

---
+++ frontend/actions/routes_actions.js
@@ -0,0 +1,26 @@
+import * as RoutesApiUtil from '../util/routes_api_util';
+
+export const RECEIVE_ALL_ROUTES = 'RECEIVE_ALL_ROUTES';
+export const RECEIVE_ROUTE = 'RECEIVE_ROUTE';
+
+const receiveAllRoutes = routes => ({
+  type: RECEIVE_ALL_ROUTES,
+  routes
+});
+
+const receiveRoute = route => ({
+  type: RECEIVE_ROUTE,
+  route
+});
+
+export const requestAllRoutes = () => dispatch => {
+  return RoutesApiUtil.fetchAllRoutes().then(routes => {
+    dispatch(receiveAllRoutes(routes));
+  });
+};
+
+export const createRoute = route =>  dispatch => {
+  return RoutesApiUtil.createRoute().then(route => {
+    dispatch(receiveRoute(route));
+  });
+};
