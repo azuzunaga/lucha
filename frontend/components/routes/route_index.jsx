76bac83ce928e288db797004b5468c3454b301ab
Americo <ame.zuz@gmail.com>
2018-04-10 15:21:26 -0700

    Implement route detail skeleton

--- frontend/components/routes/route_index.jsx
+++ frontend/components/routes/route_index.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import { withRouter, Link } from 'react-router-dom';
+import RouteDetail from './route_detail';
+
 
 class RouteIndex extends React.Component {
+  componentDidMount() {
+    this.props.requestAllRoutes();
+  }
+
   render() {
+    const routes = this.props.routes;
     return (
       <div>
-        <h1>Hover on the plus button and select "Create a route"</h1>
+        <h1>My Routes</h1>
+        <ul>
+          {routes.map(route => <RouteDetail key={route.id} route={route} />)}
+        </ul>
       </div>
     );
   }
