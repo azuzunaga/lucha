d2a8c564da9826b1824e667e34356a1ed14f016c
Americo <ame.zuz@gmail.com>
2018-04-10 13:09:19 -0700

    Add routes reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 
 import sessionReducer from './session_reducer';
 import errorsReducer from './errors_reducer';
+import routesReducer from './routes_reducer';
 
 const rootReducer = combineReducers({
   session: sessionReducer,
   errors: errorsReducer,
+  routes: routesReducer,
 });
 
 export default rootReducer;
