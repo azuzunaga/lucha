b56713400ecdb5a81ae06de5600fd59c21560a8b
Americo <ame.zuz@gmail.com>
2018-04-10 10:56:09 -0700

    Create actions for the routes controller

--- app/controllers/api/routes_controller.rb
+++ app/controllers/api/routes_controller.rb
@@ -1,11 +1,22 @@
 class Api::RoutesController < ApplicationController
+  before_action :require_logged_in
+
   def index
+    @routes = Route.find_by(author_id: current_user.id)
   end
 
   def show
+    @route = Route.find(params[:route_id])
   end
 
   def create
+    @route = Route.new(route_params)
+
+    if @route.save
+      render :index
+    else
+      render json: @route, status: :unprocessable_entity
+    end
   end
 
   private
