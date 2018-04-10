947e1e5c0d7198243ceb3596671054626f48bd7c
Americo <ame.zuz@gmail.com>
2018-04-10 10:36:17 -0700

    Add user association

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,2 +1,14 @@
 class Route < ApplicationRecord
+  validates :name,
+            :description,
+            :author_id,
+            :polyline,
+            :image_url,
+            :distance,
+            :elevation,
+            :duration, presence: true
+
+  belongs_to :author,
+             class_name: :User,
+             foreign_key: :author_id
 end
