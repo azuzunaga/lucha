19b942771efc582f9d196a35f254b0aa96ad7ba9
Americo <ame.zuz@gmail.com>
2018-04-10 10:46:01 -0700

    Configure routes routes routes routes routes routes routes routes routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,20 @@
 Rails.application.routes.draw do
+  namespace :api do
+    get 'routes/index'
+  end
+
+  namespace :api do
+    get 'routes/show'
+  end
+
+  namespace :api do
+    get 'routes/create'
+  end
+
   namespace :api, defaults: { format: :json } do
     resource :user, only: [:create]
     resource :session, only: [:create, :destroy, :show]
+    resources :routes, only: [:create, :index, :show]
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
