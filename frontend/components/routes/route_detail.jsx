266bb94e762d43cd4c2614e12b0f57d1ac7a155a
Americo <ame.zuz@gmail.com>
2018-04-10 16:10:04 -0700

    Add more details

--- frontend/components/routes/route_detail.jsx
+++ frontend/components/routes/route_detail.jsx
@@ -11,11 +11,27 @@ const RouteDetail = ({ route }) => {
   return (
     <li>
       <div className="route-detail-component">
-        <div className="image-wrapper"
+        <div className="route-detail-image"
           style={{backgroundImage: `url(${safeUrl})`}}>
         </div>
-        <h2>{route.title}</h2>
-
+        <h2 className="route-detail-title">{route.title}</h2>
+        <div className="route-detail-stats">
+          <div className="route-detail-distance">
+            <h3>{route.distance}</h3>
+            <h4>Distance</h4>
+          </div>
+          <div className="route-detail-elevation">
+            <h3>{route.elevation}</h3>
+            <h4>Elevation Gain</h4>
+          </div>
+          <div className="route-detail-duration">
+            <h4>Est. Moving Time</h4>
+            <h5>{route.duration}</h5>
+          </div>
+          <div className="route-detail-creation">
+            <h6>{route.createdAt}</h6>
+          </div>
+        </div>
       </div>
     </li>
   );
