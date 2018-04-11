f4b773d271bd008e8ee4921d2fb9a10d23c71332
Americo <ame.zuz@gmail.com>
2018-04-10 20:17:35 -0700

    Keep random stuff in here

---
+++ frontend/util/random.js
@@ -0,0 +1,94 @@
+
+https://maps.googleapis.com/maps/api/staticmap?size=570x190&maptype=roadmap&
+&markers=anchor:center|icon:https://goo.gl/ucHdze|41.439874006704436,-72.78503965087754&
+&markers=anchor:center|icon:https://goo.gl/tymZ5q|41.43700749861527,-72.78074860540494&
+&style=feature:administrative.land_parcel|visibility:off&
+&style=feature:administrative.neighborhood|visibility:off&
+&style=feature:landscape.man_made|element:geometry.fill|color:0xe5e1da&
+&style=feature:landscape.natural.terrain|element:geometry.fill|color:0xc1cbb3&
+&style=feature:poi|element:labels.text|visibility:off&
+&style=feature:poi.attraction|visibility:off&
+&style=feature:poi.business|visibility:off&
+&style=feature:poi.government|visibility:off&
+&style=feature:poi.medical|visibility:off&
+&style=feature:poi.park|element:geometry.fill|color:0xc1dead&
+&style=feature:poi.place_of_worship|visibility:off&
+&style=feature:poi.school|visibility:off&
+&style=feature:poi.sports_complex|visibility:off&
+&style=feature:road|element:labels|visibility:off&
+&style=feature:road.arterial|element:labels|visibility:on&
+&style=feature:road.highway|element:geometry.fill|color:0xababab&
+&style=feature:road.highway|element:geometry.stroke|color:0xffffff&
+&style=feature:road.local|element:labels|visibility:on&
+&style=feature:transit|visibility:off&
+&style=feature:water|element:geometry.fill|color:0x6bc1e8&
+&path=color:0xff0000ff|weight:2|enc:ov|{Fjyf{LjAuHRoDpBcGZwAD}@o@gCi@}@oAmAuCoBgCwB{AwBaAkCeAyJkBqFzEsExGaKhDyDvB{CzIaNf@kAn@kC`BgGh@sAdDkGTq@LwAEaBy@sJByCdBgHnCcOJcAIkJ?}Gt@cGl@sIj@sIWu@uJqLaB}DmAuEaAmEo@uE_@iC{@_CeAeCm@kCq@kC_C}Cw@]aBYo@a@_AeAeDaDiFuCyCmB}@yAkAuDcCmDkBuBsAiAiEuCqAiAg@u@WaACs@BiCsOy@cBYiAeAoIwJ{CiDyB_BwGkHiBeDeFuPQc@gAgAwFcEiDkB_CmC_@w@YoACs@H{A\sBL{@e@Bi@By@HoDZqCPgIDgIJqR`@_Id@wF`@_CLeAD@L?b@Bv@LtC~@bRbAxS@lBKrAc@zBk@nAaApAqAbA}DtBgBpAm@dA[jAi@dHC^{@JoBTiBZmBjAg@LI?MAId@w@bBuEzFq@RwCA_HBgDb@cMvBuHZuHTiGXcGAyNAeLFgEd@gAAkPy@cLu@uCGeCe@sEaBiD{AuBq@}FiAmGiAeDcAsFcEgJaGsEuBiAs@ISg@iB[^sD}AqBs@aCqAaCaBSwF[{BQiBBgA\mDS}A_AiD{@qEa@}@iAk@cDu@eCgAiBoAYo@e@wFg@yGl@ExIi@nIe@lHq@`BBnAXfBhAnAnAlBfCzApAnKvG|D~BnB|@`Bp@zBl@zADrHOdIQpC_@dGk@`CaAnF{Dr@aAbC{GpBeK~@aFxAgFxFmQ~@qEvBePtE{SjJqa@Hy@xBcCjBuBjAqBdBqChCcDh@aAh@gBb@qAn@{@vE}DbDaEnBOxFcApAgAd@{@xA}Df@e@EeEKwFvCQpIk@tK_AbHs@|@SrImCt@OnCAtEdAhDPhGi@dJw@bDu@dLqCz@MbBEvGt@lDz@~C\`EZnTd@p\x@bCXb@dFvBzWvBbXfBxIlAtDNxADfGKlFKfAGdEDfECzDO^~CnDjAtApC~ChFdG|EzI~I~Q`ClGfBxCjFpHhGpJ|BxEpFxHxBbEdCdH|BjHj@fDlAfSb@vCZbAdFhJfExEfKnKt\j]rOlWvJ`KxTzTzT|T`DrDfBxDfBrHxAjIhBrO`Gv_@pBrLlDxPbDfUv@zOr@bMz@fFuHUyLw@{Ii@uA_@oGkBoHkCoEoAyDnAgDfBs@pAWxB_@z@sAd@eAp@sAbDyBzCqAbAkA`@sJjByIbB{Db@eAC}De@wOiCaFy@iAT{BpAs@t@]v@_AfEkAbD{ClCyAVgBF&
+&key=AIzaSyDCQQIa8qtYmzodY0kTgG26Pn5DwQO88Co
+
+// markers
+&start: https://goo.gl/ucHdze&
+&finish: https://goo.gl/tymZ5q&
+
+// path styling
+&path=color:0xff0000ff|weight:5&
+
+// map styling
+&style=feature:administrative.land_parcel|visibility:off&
+&style=feature:administrative.neighborhood|visibility:off&
+&style=feature:landscape.man_made|element:geometry.fill|color:0xe5e1da&
+&style=feature:landscape.natural.terrain|element:geometry.fill|color:0xc1cbb3&
+&style=feature:poi|element:labels.text|visibility:off&
+&style=feature:poi.attraction|visibility:off&
+&style=feature:poi.business|visibility:off&
+&style=feature:poi.government|visibility:off&
+&style=feature:poi.medical|visibility:off&
+&style=feature:poi.park|element:geometry.fill|color:0xc1dead&
+&style=feature:poi.place_of_worship|visibility:off&
+&style=feature:poi.school|visibility:off&
+&style=feature:poi.sports_complex|visibility:off&
+&style=feature:road|element:labels|visibility:off&
+&style=feature:road.arterial|element:labels|visibility:on&
+&style=feature:road.highway|element:geometry.fill|color:0xababab&
+&style=feature:road.highway|element:geometry.stroke|color:0xffffff&
+&style=feature:road.local|element:labels|visibility:on&
+&style=feature:transit|visibility:off&
+&style=feature:water|element:geometry.fill|color:0x6bc1e8&
+
+
+{
+  name: "Wallingford Climb",
+  description: "A route around Wally-World",
+  author_id: 2,
+  polyline: "ov|{Fjyf{LjAuHRoDpBcGZwAD}@o@gCi@}@oAmAuCoBgCwB{AwBaAkCeAyJkBqFzEsExGaKhDyDvB{CzIaNf@kAn@kC`BgGh@sAdDkGTq@LwAEaBy@sJByCdBgHnCcOJcAIkJ?}Gt@cGl@sIj@sIWu@uJqLaB}DmAuEaAmEo@uE_@iC{@_CeAeCm@kCq@kC_C}Cw@]aBYo@a@_AeAeDaDiFuCyCmB}@yAkAuDcCmDkBuBsAiAiEuCqAiAg@u@WaACs@BiCsOy@cBYiAeAoIwJ{CiDyB_BwGkHiBeDeFuPQc@gAgAwFcEiDkB_CmC_@w@YoACs@H{A\sBL{@e@Bi@By@HoDZqCPgIDgIJqR`@_Id@wF`@_CLeAD@L?b@Bv@LtC~@bRbAxS@lBKrAc@zBk@nAaApAqAbA}DtBgBpAm@dA[jAi@dHC^{@JoBTiBZmBjAg@LI?MAId@w@bBuEzFq@RwCA_HBgDb@cMvBuHZuHTiGXcGAyNAeLFgEd@gAAkPy@cLu@uCGeCe@sEaBiD{AuBq@}FiAmGiAeDcAsFcEgJaGsEuBiAs@ISg@iB[^sD}AqBs@aCqAaCaBSwF[{BQiBBgA\mDS}A_AiD{@qEa@}@iAk@cDu@eCgAiBoAYo@e@wFg@yGl@ExIi@nIe@lHq@`BBnAXfBhAnAnAlBfCzApAnKvG|D~BnB|@`Bp@zBl@zADrHOdIQpC_@dGk@`CaAnF{Dr@aAbC{GpBeK~@aFxAgFxFmQ~@qEvBePtE{SjJqa@Hy@xBcCjBuBjAqBdBqChCcDh@aAh@gBb@qAn@{@vE}DbDaEnBOxFcApAgAd@{@xA}Df@e@EeEKwFvCQpIk@tK_AbHs@|@SrImCt@OnCAtEdAhDPhGi@dJw@bDu@dLqCz@MbBEvGt@lDz@~C\`EZnTd@p\x@bCXb@dFvBzWvBbXfBxIlAtDNxADfGKlFKfAGdEDfECzDO^~CnDjAtApC~ChFdG|EzI~I~Q`ClGfBxCjFpHhGpJ|BxEpFxHxBbEdCdH|BjHj@fDlAfSb@vCZbAdFhJfExEfKnKt\j]rOlWvJ`KxTzTzT|T`DrDfBxDfBrHxAjIhBrO`Gv_@pBrLlDxPbDfUv@zOr@bMz@fFuHUyLw@{Ii@uA_@oGkBoHkCoEoAyDnAgDfBs@pAWxB_@z@sAd@eAp@sAbDyBzCqAbAkA`@sJjByIbB{Db@eAC}De@wOiCaFy@iAT{BpAs@t@]v@_AfEkAbD{ClCyAVgBF",
+  image_url: "https://maps.googleapis.com/maps/api/staticmap?size=570x190&maptype=roadmap&
+  &markers=anchor:center|icon:https://goo.gl/ucHdze|41.439874006704436,-72.78503965087754&
+  &markers=anchor:center|icon:https://goo.gl/tymZ5q|41.43700749861527,-72.78074860540494&
+  &style=feature:administrative.land_parcel|visibility:off&
+  &style=feature:administrative.neighborhood|visibility:off&
+  &style=feature:landscape.man_made|element:geometry.fill|color:0xe5e1da&
+  &style=feature:landscape.natural.terrain|element:geometry.fill|color:0xc1cbb3&
+  &style=feature:poi|element:labels.text|visibility:off&
+  &style=feature:poi.attraction|visibility:off&
+  &style=feature:poi.business|visibility:off&
+  &style=feature:poi.government|visibility:off&
+  &style=feature:poi.medical|visibility:off&
+  &style=feature:poi.park|element:geometry.fill|color:0xc1dead&
+  &style=feature:poi.place_of_worship|visibility:off&
+  &style=feature:poi.school|visibility:off&
+  &style=feature:poi.sports_complex|visibility:off&
+  &style=feature:road|element:labels|visibility:off&
+  &style=feature:road.arterial|element:labels|visibility:on&
+  &style=feature:road.highway|element:geometry.fill|color:0xababab&
+  &style=feature:road.highway|element:geometry.stroke|color:0xffffff&
+  &style=feature:road.local|element:labels|visibility:on&
+  &style=feature:transit|visibility:off&
+  &style=feature:water|element:geometry.fill|color:0x6bc1e8&
+  &path=color:0xff0000ff|weight:2|enc:ov|{Fjyf{LjAuHRoDpBcGZwAD}@o@gCi@}@oAmAuCoBgCwB{AwBaAkCeAyJkBqFzEsExGaKhDyDvB{CzIaNf@kAn@kC`BgGh@sAdDkGTq@LwAEaBy@sJByCdBgHnCcOJcAIkJ?}Gt@cGl@sIj@sIWu@uJqLaB}DmAuEaAmEo@uE_@iC{@_CeAeCm@kCq@kC_C}Cw@]aBYo@a@_AeAeDaDiFuCyCmB}@yAkAuDcCmDkBuBsAiAiEuCqAiAg@u@WaACs@BiCsOy@cBYiAeAoIwJ{CiDyB_BwGkHiBeDeFuPQc@gAgAwFcEiDkB_CmC_@w@YoACs@H{A\sBL{@e@Bi@By@HoDZqCPgIDgIJqR`@_Id@wF`@_CLeAD@L?b@Bv@LtC~@bRbAxS@lBKrAc@zBk@nAaApAqAbA}DtBgBpAm@dA[jAi@dHC^{@JoBTiBZmBjAg@LI?MAId@w@bBuEzFq@RwCA_HBgDb@cMvBuHZuHTiGXcGAyNAeLFgEd@gAAkPy@cLu@uCGeCe@sEaBiD{AuBq@}FiAmGiAeDcAsFcEgJaGsEuBiAs@ISg@iB[^sD}AqBs@aCqAaCaBSwF[{BQiBBgA\mDS}A_AiD{@qEa@}@iAk@cDu@eCgAiBoAYo@e@wFg@yGl@ExIi@nIe@lHq@`BBnAXfBhAnAnAlBfCzApAnKvG|D~BnB|@`Bp@zBl@zADrHOdIQpC_@dGk@`CaAnF{Dr@aAbC{GpBeK~@aFxAgFxFmQ~@qEvBePtE{SjJqa@Hy@xBcCjBuBjAqBdBqChCcDh@aAh@gBb@qAn@{@vE}DbDaEnBOxFcApAgAd@{@xA}Df@e@EeEKwFvCQpIk@tK_AbHs@|@SrImCt@OnCAtEdAhDPhGi@dJw@bDu@dLqCz@MbBEvGt@lDz@~C\`EZnTd@p\x@bCXb@dFvBzWvBbXfBxIlAtDNxADfGKlFKfAGdEDfECzDO^~CnDjAtApC~ChFdG|EzI~I~Q`ClGfBxCjFpHhGpJ|BxEpFxHxBbEdCdH|BjHj@fDlAfSb@vCZbAdFhJfExEfKnKt\j]rOlWvJ`KxTzTzT|T`DrDfBxDfBrHxAjIhBrO`Gv_@pBrLlDxPbDfUv@zOr@bMz@fFuHUyLw@{Ii@uA_@oGkBoHkCoEoAyDnAgDfBs@pAWxB_@z@sAd@eAp@sAbDyBzCqAbAkA`@sJjByIbB{Db@eAC}De@wOiCaFy@iAT{BpAs@t@]v@_AfEkAbD{ClCyAVgBF&
+  &key=AIzaSyDCQQIa8qtYmzodY0kTgG26Pn5DwQO88Co",
+  distance: 24.4,
+  elevation: 1255,
+  duration: 9945,
+}
+
+
+https://maps.googleapis.com/maps/api/staticmap?size=570x190&maptype=roadmap&&markers=anchor:center|icon:https://goo.gl/ucHdze|41.439874006704436,-72.78503965087754&&markers=anchor:center|icon:https://goo.gl/tymZ5q|41.43700749861527,-72.78074860540494&&key=AIzaSyDCQQIa8qtYmzodY0kTgG26Pn5DwQO88Co
