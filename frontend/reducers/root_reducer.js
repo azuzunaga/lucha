0e7a64b7c13d58b39f0550c1a99f60bfe23a5277
Americo <ame.zuz@gmail.com>
2018-04-11 16:58:48 -0700

    Add activities reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux';
 import sessionReducer from './session_reducer';
 import errorsReducer from './errors_reducer';
 import routesReducer from './routes_reducer';
+import activitiesReducer from './activities_reducer';
 
 const rootReducer = combineReducers({
   session: sessionReducer,
   errors: errorsReducer,
   routes: routesReducer,
+  activities: activitiesReducer,
 });
 
 export default rootReducer;
