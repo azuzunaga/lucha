7d158c43627f34e85bb5469007b6388e744dd8dd
Americo <ame.zuz@gmail.com>
2018-04-11 14:04:19 -0700

    Begin new activity MVP

--- frontend/components/activities/new_activity.jsx
+++ frontend/components/activities/new_activity.jsx
@@ -1,1 +1,10 @@
+import React from 'react';
+import { withRouter, Link } from 'react-router-dom';
 
+import RouteIndexContainer from '../routes/route_index_container';
+
+class NewActivity extends React.Component {
+
+}
+
+export default withRouter(NewActivity);
---
+++ frontend/components/activities/new_activity_container.jsx
@@ -0,0 +1,23 @@
+import { connect } from 'react-redux';
+import React from 'react';
+
+import { selectAllRoutes } from '../../reducers/selectors';
+import { createActivity } from '../../actions/activities_actions';
+
+import NewActivity from './new_activity';
+
+const mapStateToProps = state => {
+  return {
+    currentUser: state.session.currentUser,
+    errors: state.errors.activities
+  };
+};
+
+const mapDispatchToProps = dispatch => ({
+  processActivityForm: activity => dispatch(createActivity(activity))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(NewActivity);
