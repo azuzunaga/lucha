41137c55f6f76277a3bdafa66b65309e1c5c8f32
Americo <ame.zuz@gmail.com>
2018-04-11 16:36:17 -0700

    Generate activities controller

---
+++ app/controllers/api/activities_controller.rb
@@ -0,0 +1,7 @@
+class Api::ActivitiesController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,9 @@
 Rails.application.routes.draw do
-  namespace :api do
-    get 'routes/index'
-  end
-
-  namespace :api do
-    get 'routes/show'
-  end
-
-  namespace :api do
-    get 'routes/create'
-  end
-
   namespace :api, defaults: { format: :json } do
     resource :user, only: [:create]
     resource :session, only: [:create, :destroy, :show]
     resources :routes, only: [:create, :index]
+    resources :activities, only: [:create, :index]
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
