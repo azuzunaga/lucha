a9f50e67d4acf0d04ddb6612fabb74eed182a8f6
Americo <ame.zuz@gmail.com>
2018-04-10 19:16:53 -0700

    Add url path constants

---
+++ frontend/util/static_map_constants.js
@@ -0,0 +1,8 @@
+export const prefix = "https://maps.googleapis.com/maps/api/staticmap"
+export const sizeOption = "size=570x190";
+export const mapTypeOption = "maptype=roadmap";
+export const startMarkerOption = "markers=anchor:center|icon:https://goo.gl/ucHdze|";
+export const endMarkerOption = "markers=anchor:center|icon:https://goo.gl/tymZ5q|";
+export const keyOption = "key=AIzaSyDCQQIa8qtYmzodY0kTgG26Pn5DwQO88Co";
+
+export const mapStyleOption = "style=feature:administrative.land_parcel|visibility:off&style=feature:administrative.neighborhood|visibility:off&style=feature:landscape.man_made|element:geometry.fill|color:0xe5e1da&style=feature:landscape.natural.terrain|element:geometry.fill|color:0xc1cbb3&style=feature:poi|element:labels.text|visibility:off&style=feature:poi.attraction|visibility:off&style=feature:poi.business|visibility:off&style=feature:poi.government|visibility:off&style=feature:poi.medical|visibility:off&style=feature:poi.park|element:geometry.fill|color:0xc1dead&style=feature:poi.place_of_worship|visibility:off&style=feature:poi.school|visibility:off&style=feature:poi.sports_complex|visibility:off&style=feature:road|element:labels|visibility:off&style=feature:road.arterial|element:labels|visibility:on&style=feature:road.highway|element:geometry.fill|color:0xababab&style=feature:road.highway|element:geometry.stroke|color:0xffffff&style=feature:road.local|element:labels|visibility:on&style=feature:transit|visibility:off&style=feature:water|element:geometry.fill|color:0x6bc1e8";
