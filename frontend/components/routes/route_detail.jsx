3438c30a0213355267e9e61e3ae87dfeee997fb2
Americo <ame.zuz@gmail.com>
2018-04-10 17:53:36 -0700

    Complete route detail html

--- frontend/components/routes/route_detail.jsx
+++ frontend/components/routes/route_detail.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router';
+import {
+  elevationFormatter,
+  distanceFormatter,
+  durationFormatter,
+  timeFormatter,
+} from '../../util/map-utils';
 
 const RouteDetail = ({ route }) => {
   const safeUrl = encodeURI(route.imageUrl);
@@ -14,23 +20,25 @@ const RouteDetail = ({ route }) => {
         <div className="route-detail-image"
           style={{backgroundImage: `url(${safeUrl})`}}>
         </div>
-        <h2 className="route-detail-title">{route.title}</h2>
         <div className="route-detail-stats">
-          <div className="route-detail-distance">
-            <h3>{route.distance}</h3>
-            <h4>Distance</h4>
-          </div>
-          <div className="route-detail-elevation">
-            <h3>{route.elevation}</h3>
-            <h4>Elevation Gain</h4>
-          </div>
+          <h2 className="route-detail-title">{route.title}</h2>
+          <ul className="route-main-stats">
+            <li className="route-detail-distance">
+              <h3>{distanceFormatter(route.distance)}</h3>
+              <h4>Distance</h4>
+            </li>
+            <li className="route-detail-elevation">
+              <h3>{elevationFormatter(route.elevation)}</h3>
+              <h4>Elevation Gain</h4>
+            </li>
+          </ul>
           <div className="route-detail-duration">
             <h4>Est. Moving Time</h4>
-            <h5>{route.duration}</h5>
+            <h5>{durationFormatter(route.duration)}</h5>
           </div>
-          <div className="route-detail-creation">
-            <h6>{route.createdAt}</h6>
-          </div>
+        </div>
+        <div className="route-detail-creation">
+          <h6>Created on {timeFormatter(route.createdAt)}</h6>
         </div>
       </div>
     </li>
