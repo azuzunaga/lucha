954f6d635b07da734311c58eabfaf94f8393541d
Americo <ame.zuz@gmail.com>
2018-04-11 09:38:25 -0700

    Add activity type filtering

--- frontend/components/routes/route_index.jsx
+++ frontend/components/routes/route_index.jsx
@@ -4,12 +4,24 @@ import RouteDetail from './route_detail';
 
 
 class RouteIndex extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedOption: 'bicycling'
+    };
+  }
+
   componentDidMount() {
     this.props.requestAllRoutes();
   }
 
+  sportType(sport) {
+    this.setState({selectedOption: sport});
+  }
+
   render() {
-    const routes = this.props.routes;
+    const allRoutes = this.props.routes;
+    const routes = allRoutes.filter(e => e.sport === this.state.selectedOption);
     return (
       <div className="route-index-component">
         <div className="route-index-header">
@@ -22,13 +34,18 @@ class RouteIndex extends React.Component {
           <ul className="route-detail-type">
             <li>
               <input type="radio" name="type"
-                value="ride" checked
+                value="bicycling"
+                defaultChecked={this.state.selectedOption === 'bicycling'}
+                onChange={this.sportType.bind(this, "bicycling")}
               id="cycle" />
               <label htmlFor="cycle" className="left-label">Cycling</label>
             </li>
             <li>
               <input type="radio" name="type"
-              value="run" id="run" />
+                value="running"
+                defaultChecked={this.state.selectedOption === 'running'}
+                onChange={this.sportType.bind(this, "running")}
+              id="run" />
               <label htmlFor="run" className="right-label">Running</label>
             </li>
           </ul>
