9aacbd11b0c8ba1d3468032694057271b0d37106
Americo <ame.zuz@gmail.com>
2018-04-13 01:19:21 -0700

    Create activity index

--- frontend/components/activities/activity_index.jsx
+++ frontend/components/activities/activity_index.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import { withRouter, Link } from 'react-router-dom';
 
+import ActivityDetail from './activity_detail';
+
 class ActivityIndex extends React.Component {
+
+  componentDidMount() {
+    this.props.requestAllActivities();
+  }
+
   render() {
+    const activities = this.props.activities;
     return (
-      <h1>Antes muerto que sencillo</h1>
+      <div className="activity-index-component">
+        <div className="activity-detail-container">
+          <ul className="activity-detail-cards">
+            {activities.reverse().map(activity =>
+              <ActivityDetail
+                key={activity.id}
+                activity={activity}
+                firstName={this.props.currentUser.firstName}
+                lastName={this.props.currentUser.lastName}
+              />
+            )}
+          </ul>
+        </div>
+      </div>
     );
   }
 }
